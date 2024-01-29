f2a4d1fc52c974a791482bff73390214b09a4bcf
Fernando Rocha <fernando.rocha@prodatamobility.com.br>
2024-01-29 18:37:23 -0300

    Conflite resolved

--- scripts-db/Customer.sql
+++ scripts-db/Customer.sql
@@ -13,19 +13,11 @@ use QuickBiteStaging;
 --drop table Orders;
 --drop table MoneyOrder;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-create table Custormers (
-	Id uniqueidentifier primary key,
-=======
-=======
 
 --CREATE TABLES
->>>>>>> template
 create table Customer (
 	Id integer IDENTITY(1,1) PRIMARY KEY,
 	Uid uniqueidentifier unique,
->>>>>>> template
 	Name varchar(200) null,
 	Document varchar(20) null,
 	Email varchar(200) null,
@@ -84,13 +76,13 @@ insert into Customer(Uid, Name, Document, Email, CreatedAt)
 values ('43c019c6-b7df-453c-8b2d-982c886c6b80', 'Naruto Uzumaki', '32145698710', 'naruto@teste.com', GETDATE());
 
 insert into Category(Uid, Name, Description, CreatedAt)
-values ('bf9dfa9a-da2e-423c-b145-b84b62497909', 'Lanches', 'Os mais deliciosos lanches da região.', GETDATE());
+values ('bf9dfa9a-da2e-423c-b145-b84b62497909', 'Lanches', 'Os mais deliciosos lanches da regiï¿½o.', GETDATE());
 insert into Category(Uid, Name, Description, CreatedAt)
 values ('7dd939b6-4b65-4474-bc04-b0c867597726', 'Bebidas', 'Diversas bebidas para acompanhar seu lanche.', GETDATE());
 insert into Category(Uid, Name, Description, CreatedAt)
 values ('44266b4d-2614-468d-9789-56e2bd9e6d23', 'Acompanhamentos', 'Diversos acompanhamentos para seu lanche.', GETDATE());
 insert into Category(Uid, Name, Description, CreatedAt)
-values ('96153509-b6ce-46f8-aac1-d135d2a2c37a', 'Sobremesas', 'Para finalizar sua experiência.', GETDATE());
+values ('96153509-b6ce-46f8-aac1-d135d2a2c37a', 'Sobremesas', 'Para finalizar sua experiï¿½ncia.', GETDATE());
 
 insert into Product(Uid, Name, Description, CategoryUid, Enable, Sellable, UnitPrice, ComboPrice, CreatedAt)
 values ('792a80f0-bbc3-48f6-b9f9-8fb08df717b2', 'Big Mac', 'O tradicional lanche.', 'bf9dfa9a-da2e-423c-b145-b84b62497909', 1, 1, 9.00, 6.50, GETDATE());
@@ -99,9 +91,7 @@ values ('9c652bba-9cc0-44a9-b3e6-3eec7d43139f', 'Coca Cola', 'O tradicional bebi
 
 --QUERIES
 select *
-<<<<<<< HEAD
-from Custormers with(nolock);
-=======
+
 from Customer with(nolock);
 
 select *
@@ -112,10 +102,6 @@ from Product with(nolock);
 
 select *
 from Orders with(nolock);
-<<<<<<< HEAD
->>>>>>> template
-=======
 
 select *
 from MoneyOrder with(nolock);
->>>>>>> template
